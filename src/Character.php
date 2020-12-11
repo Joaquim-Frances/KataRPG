b45eebaf7cbfa39e70d4f5e55d5eadebba7c9ab4
Joaquim-Frances <quimifrances@gmail.com>
2020-12-11 18:02:21 +0100

    2cond step iteration one, test passed

--- src/Character.php
+++ src/Character.php
@@ -4,5 +4,35 @@ namespace App;
 
 class Character
 {
-   
+   private int $health = 1000;
+   private int $level = 1;
+   private bool $isAlive = true;
+
+   public function getHealth() : int
+    { 
+       return $this->health;
+    }
+    public function setHealth($newHealth) : void
+    {
+        $this->health = $newHealth;
+    }
+    
+    public function getLevel() : int
+    { 
+       return $this->level;
+    }
+    
+    public function getIsAlive() : bool
+    { 
+       return $this->isAlive;
+    }
+
+    public function attack($target, $damage) : void
+    {
+        $targetHealth = $target->health - $damage;
+        $target->setHealth($targetHealth);
+    }
+
+
+
 } 
--- tests/CharacterTest.php
+++ tests/CharacterTest.php
@@ -12,7 +12,26 @@ class CharacterTest extends TestCase
 
 	public function test_Health_starting_at_1000()
 	{
-	
+		$character = new Character;
+		$healthCharacter = $character->getHealth();
+		$levelCharacter = $character->getLevel();
+		$AliveStatus = $character->getIsAlive();
+
+		$this->assertEquals($healthCharacter, 1000);
+		$this->assertEquals($levelCharacter, 1);
+		$this->assertEquals($AliveStatus, true);
+	}
+
+	public function test_Character_Can_Damage_Other_Characters(){
+
+	//given
+		$attacker = new Character;
+		$enemy = new Character;
+	//when
+		$attacker->attack($enemy, 100);
+		$targetHealth = $enemy->getHealth();
+	//then
+		$this->assertEquals($targetHealth, 900);
 	}
 
 		
